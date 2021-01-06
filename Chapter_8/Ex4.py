84e177dccce745df6a115d2490bcb4169a6fdae9
adrinmdq <74638802+adrinmdq@users.noreply.github.com>
2021-01-06 00:00:40 -0300

    List Exercise 4 updated

--- Chapter_8/Ex4.py
+++ Chapter_8/Ex4.py
@@ -2,21 +2,20 @@
 # Search if the words below are on the list or not.
 # If they are not, add it to the list. Sort it and print it in alphabetical order.
 
-wordlist = ['Arise', 'But', 'It', 'Juliet', 'Who', 'already',
-'and', 'breaks', 'east', 'envious', 'fair', 'grief',
-'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft',
-'sun', 'the', 'through', 'what', 'window',
-'with', 'yonder']
+# Result expected: ['Arise', 'But', 'It', 'Juliet', 'Who', 'already',
+# 'and', 'breaks', 'east', 'envious', 'fair', 'grief',
+# 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft',
+# 'sun', 'the', 'through', 'what', 'window',
+# 'with', 'yonder']
 
 fhand = open('romeo.txt')
 nlist = []
 for line in fhand:
     nwords = line.split()
-    nlist.extend(nwords)
-for word in wordlist:
-    try:
-        nlist.index(word)
-    except:
-        nlist.append(word)
+    for word in nwords:
+        try:
+            nlist.index(word)
+        except:
+            nlist.append(word)
 nlist.sort()
 print(nlist)
